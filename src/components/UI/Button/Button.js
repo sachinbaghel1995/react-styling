fc8bb5a69b49718cb0bf743a9b9c5cc5ae4af587
Sachin Baghel <baghel.sachin1996@gmail.com>
2023-03-03 19:20:33 +0530

    styling

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './button.css';
+
+const Button = props => {
+  return (
+    <button type={props.type} className="button" onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
